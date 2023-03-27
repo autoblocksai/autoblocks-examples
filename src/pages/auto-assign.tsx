1555f8de7aaa99bfb6f2b719ed4d4d670158f112
Adam Nolte <ajnolte12@gmail.com>
2023-03-27 16:57:25 -0500

    Fix typo

--- src/pages/auto-assign.tsx
+++ src/pages/auto-assign.tsx
@@ -130,7 +130,7 @@ export default function Task() {
   };
   return (
     <main className="flex flex-col items-center p-8 max-w-4xl mx-auto">
-      <h1 className="text-2xl mb-4">Autoblocks Task Example</h1>
+      <h1 className="text-2xl mb-4">Autoblocks Auto Assign Example</h1>
       <Nav />
       <form action="#" className="relative w-full">
         <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
