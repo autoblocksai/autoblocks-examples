a84c85186114667fc77615ad932d50ac0eb3d753
Adam Nolte <ajnolte12@gmail.com>
2023-03-27 12:54:19 -0500

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '~/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Autoblocks Example</title>
+        <meta name="description" content="Examples of how to use autoblocks" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/email.tsx
+++ src/pages/email.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-export default function Home() {
+export default function Email() {
   return (
     <main className="flex flex-col items-center p-8">
-      <h1 className="text-2xl mb-4">Autoblocks Examples</h1>
+      <h1 className="text-2xl mb-4">Autoblocks Email Example</h1>
       <ul className="text-blue-700 text-lg list-disc">
         <li>
           <Link href="/chat">Chat Example</Link>
