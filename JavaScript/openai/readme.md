bd4491342b55a73222889d5a0dd4b2ad5d903a77
Adam Nolte <adam@autoblocks.ai>
2023-09-07 16:47:58 -0500

    Remove submodule

---
+++ JavaScript/openai/readme.md
@@ -0,0 +1,42 @@
+<p align="center">
+  <img src="https://app.autoblocks.ai/images/logo.png" width="300px">
+</p>
+
+# Node Autoblocks Example
+
+Example Node application using [Autoblocks](https://www.autoblocks.ai).
+
+## Quick Start
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Sign up for Autoblocks
+
+Sign up for an Autoblocks account at https://app.autoblocks.ai and grab your ingestion key from [settings](https://app.autoblocks.ai/settings/api-keys).
+
+### Set environment variables
+
+Create a `.env` file in the root directory of the project with the following environment variables:
+
+```
+OPENAI_API_KEY=<your-api-key>
+AUTOBLOCKS_INGESTION_KEY=<your-ingestion-key>
+```
+
+### Run the app
+
+```
+npm run start
+```
+
+### View logs in Autoblocks
+
+After you run the app, you will see a link to view the trace in your console. You can also navigate directly to the [explore page](https://app.autoblocks.ai/explore) to see the trace.
+
+## More Information
+
+For more information on how to use Autoblocks, visit the [Autoblocks documentation](https://docs.autoblocks.ai/).
---
+++ JavaScript/openai/src/index.js
@@ -0,0 +1,74 @@
+import crypto from 'crypto';
+import OpenAI from 'openai';
+import { AutoblocksTracer } from '@autoblocks/client';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const traceId = crypto.randomUUID();
+const ab = new AutoblocksTracer(process.env.AUTOBLOCKS_INGESTION_KEY, {
+  // These apply to every call of ab.sendEvent() so we don't have to repeat them
+  traceId,
+  properties: {
+    source: 'NODE_EXAMPLE',
+  },
+});
+
+async function run() {
+  console.log('Running example...');
+
+  const openAIRequest = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are a helpful assistant.' +
+          'You answer questions about a software product named Acme.',
+      },
+      {
+        role: 'user',
+        content: 'How do I sign up?',
+      },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: false,
+    n: 1,
+  };
+
+  await ab.sendEvent('ai.request', {
+    properties: {
+      ...openAIRequest,
+      provider: 'openai',
+    },
+  });
+
+  try {
+    const now = Date.now();
+    const response = await openai.chat.completions.create(openAIRequest);
+    await ab.sendEvent('ai.response', {
+      properties: {
+        response,
+        provider: 'openai',
+        latency: Date.now() - now,
+      },
+    });
+  } catch (error) {
+    await ab.sendEvent('ai.error', {
+      properties: {
+        error,
+        provider: 'openai',
+      },
+    });
+  }
+
+  console.log(
+    `Finished running example. View the trace at https://app.autoblocks.ai/explore/trace/${traceId}`
+  );
+}
+
+run();
