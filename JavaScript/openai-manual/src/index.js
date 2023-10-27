865d4feb70fe3e9ea016d0c68398a265ce30258e
Nicole White <nicole@autoblocks.ai>
2023-10-26 22:02:59 -0400

    Rename to tracer (#23)

--- JavaScript/openai-manual/src/index.js
+++ JavaScript/openai-manual/src/index.js
@@ -6,10 +6,9 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const traceId = crypto.randomUUID();
-const ab = new AutoblocksTracer(process.env.AUTOBLOCKS_INGESTION_KEY, {
+const tracer = new AutoblocksTracer(process.env.AUTOBLOCKS_INGESTION_KEY, {
   // These apply to every call of ab.sendEvent() so we don't have to repeat them
-  traceId,
+  traceId: crypto.randomUUID(),
   properties: {
     provider: 'openai',
     source: 'NODE_EXAMPLE',
@@ -41,21 +40,21 @@ async function run() {
     n: 1,
   };
 
-  await ab.sendEvent('ai.request', {
+  await tracer.sendEvent('ai.request', {
     properties: openAIRequest,
   });
 
   try {
     const now = Date.now();
     const response = await openai.chat.completions.create(openAIRequest);
-    await ab.sendEvent('ai.response', {
+    await tracer.sendEvent('ai.response', {
       properties: {
         response,
         latency: Date.now() - now,
       },
     });
   } catch (error) {
-    await ab.sendEvent('ai.error', {
+    await tracer.sendEvent('ai.error', {
       properties: {
         error,
       },
@@ -63,7 +62,7 @@ async function run() {
   }
 
   console.log(
-    `Finished running example. View the trace at https://app.autoblocks.ai/explore/trace/${traceId}`,
+    `Finished running example. View the trace at https://app.autoblocks.ai/explore/trace/${tracer.traceId}`,
   );
 }
 
