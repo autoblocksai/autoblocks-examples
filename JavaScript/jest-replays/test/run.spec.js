0f2337dffb4e398e90d25231f48aa16fadefb678
Nicole White <nicole@autoblocks.ai>
2023-11-01 10:49:11 -0400

    Fix conflict in trace IDs between jest and pytest replays

--- JavaScript/jest-replays/test/run.spec.js
+++ JavaScript/jest-replays/test/run.spec.js
@@ -26,9 +26,10 @@ describe('run', () => {
       // If the input is too long to use as an identifier, we could
       // also update testCases to be a list of objects where one
       // field is the name of the test case and the other is the input.
-      const traceId = input;
 
-      await run({ input, traceId });
+      // Also prefix the traceId with 'jest -' since we're running pytest
+      // replays on the same branch and need to avoid conflicts in traceIds.
+      await run({ input, traceId: `jest - ${input}` });
     });
   });
 });
--- Python/pytest-replays/test_main.py
+++ Python/pytest-replays/test_main.py
@@ -11,4 +11,12 @@ test_cases = [
 
 @pytest.mark.parametrize("content", test_cases)
 def test_main(content: str):
-    run(content=content, trace_id=content)
+    # Use the input as the trace_id; this will make it so
+    # we can compare the runs against each other across replays.
+    # If the input is too long to use as an identifier, we could
+    # also update test_cases to be a list of dictionaries where one
+    # field is the name of the test case and the other is the input.
+
+    # Also prefix the trace_id with 'pytest -' since we're running jest
+    # replays on the same branch and need to avoid conflicts in trace IDs.
+    run(content=content, trace_id=f"pytest - {content}")
