3b538ef10391cd14ff68399a1daeb5295975a213
Nicole White <nicole@autoblocks.ai>
2024-01-07 11:36:16 -0500

    Update local prompt manager example (#76)

--- JavaScript/prompt-sdk/src/index.ts
+++ JavaScript/prompt-sdk/src/index.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto';
 import OpenAI from 'openai';
 import { AutoblocksTracer } from '@autoblocks/client';
-import { AutoblocksPromptBuilder } from '@autoblocks/client/prompts';
+import { AutoblocksLocalPromptManager } from '@autoblocks/client/prompts';
 import { ChatCompletionCreateParamsNonStreaming } from 'openai/resources';
 
 const openai = new OpenAI({
@@ -12,36 +12,46 @@ const tracer = new AutoblocksTracer(process.env.AUTOBLOCKS_INGESTION_KEY, {
   traceId: crypto.randomUUID(),
 });
 
-enum PromptTrackingId {
-  FEATURE_A = 'feature-a',
-}
-
 async function run() {
   // Use a span ID to group together the request + response/error events
   const spanId = crypto.randomUUID();
 
-  const builder = new AutoblocksPromptBuilder(PromptTrackingId.FEATURE_A);
+  const manager = new AutoblocksLocalPromptManager({
+    id: 'text-summarization',
+  });
 
   const params: ChatCompletionCreateParamsNonStreaming = {
     model: 'gpt-3.5-turbo',
     messages: [
       {
         role: 'system',
-        content: builder.build('text-summarization/system', {
-          languageRequirement: builder.build('common/language', {
-            language: 'Spanish',
-          }),
-          toneRequirement: builder.build('common/tone', {
-            tone: 'formal',
-          }),
+        content: manager.render({
+          template: 'system',
+          params: {
+            languageRequirement: manager.render({
+              template: 'util/language',
+              params: {
+                language: 'Spanish',
+              },
+            }),
+            toneRequirement: manager.render({
+              template: 'util/tone',
+              params: {
+                tone: 'formal',
+              },
+            }),
+          },
         }),
       },
       {
         role: 'user',
         content: ['doc1', 'doc2', 'etc']
           .map((doc) => {
-            return builder.build('text-summarization/document', {
-              document: doc,
+            return manager.render({
+              template: 'document',
+              params: {
+                document: doc,
+              },
             });
           })
           .join('\n\n'),
@@ -62,7 +72,7 @@ async function run() {
     properties: {
       response,
     },
-    promptTracking: builder.usage(),
+    promptTracking: manager.track(),
   });
 }
 
