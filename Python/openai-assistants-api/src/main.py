6cd9563ea04882b3f6c8dfae0aa01334fefa193a
Adam Nolte <adam@autoblocks.ai>
2024-02-25 14:05:37 -0600

    Add comments

--- Python/openai-assistants-api/src/main.py
+++ Python/openai-assistants-api/src/main.py
@@ -17,6 +17,9 @@ tracer = AutoblocksTracer(
 
 
 def main():
+    print("Running example...")
+
+    # Create the assistant
     assistant_create_params = dict(
         name="Math Tutor",
         instructions="You are a personal math tutor. Write and run code to answer math questions.",
@@ -25,14 +28,18 @@ def main():
     )
     tracer.send_event("ai.assistant.create", properties=assistant_create_params)
     assistant = client.beta.assistants.create(**assistant_create_params)
+    # All future events will have the assistant_id and model properties
     tracer.update_properties(dict(assistant_id=assistant.id, model=assistant.model))
     tracer.send_event("ai.assistant.created")
 
+    # Create a new thread
     tracer.send_event("ai.assistant.thread.create")
     thread = client.beta.threads.create()
+    # All future events will have the thread_id property
     tracer.update_properties(dict(thread_id=thread.id))
     tracer.send_event("ai.assistant.thread.created")
 
+    # Create a user message
     message_create_params = dict(
         thread_id=thread.id,
         role="user",
@@ -46,6 +53,7 @@ def main():
         "ai.assistant.thread.message.created", properties=dict(message_id=message.id)
     )
 
+    # Run the assistant
     run_params = dict(
         thread_id=thread.id,
         assistant_id=assistant.id,
@@ -53,24 +61,28 @@ def main():
     )
     tracer.send_event("ai.assistant.thread.run.create", properties=run_params)
     run = client.beta.threads.runs.create(**run_params)
+    # All future events will have the run_id property
     tracer.update_properties(dict(run_id=run.id))
     tracer.send_event("ai.assistant.thread.run.created")
 
     run_completed = False
 
+    # Wait for run to complete
     while not run_completed:
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
         if run.status == "completed":
             run_completed = True
 
+    # Log the steps of the run
     run_steps = client.beta.threads.runs.steps.list(thread_id=thread.id, run_id=run.id)
-
     for step in run_steps:
         tracer.send_event(
             f"ai.assistant.thread.run.step.{step.type}",
+            # Autoblocks uses run_ids and parent_run_ids to construct the trace tree view
             properties=dict(run_id=step.id, parent_run_id=step.run_id),
         )
 
+    # Log the output of the run
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     tracer.send_event(
         "ai.assistant.thread.run.completed",
