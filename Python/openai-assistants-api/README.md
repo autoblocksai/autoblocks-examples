870f84c70640895be230b0912b9d20d08db92783
Adam Nolte <adam@autoblocks.ai>
2024-02-25 14:21:13 -0600

    Add prompt example

---
+++ Python/openai-assistants-api/README.md
@@ -0,0 +1,77 @@
+<!-- banner start -->
+<p align="center">
+  <img src="https://app.autoblocks.ai/images/logo.png" width="300px">
+</p>
+
+<p align="center">
+  📚
+  <a href="https://docs.autoblocks.ai/">Documentation</a>
+  &nbsp;
+  •
+  &nbsp;
+  🖥️
+  <a href="https://app.autoblocks.ai/">Application</a>
+  &nbsp;
+  •
+  &nbsp;
+  🏠
+  <a href="https://www.autoblocks.ai/">Home</a>
+</p>
+<!-- banner end -->
+
+<!-- getting started start -->
+
+## Getting started
+
+- Sign up for an Autoblocks account at https://app.autoblocks.ai
+- Grab your Autoblocks ingestion key from https://app.autoblocks.ai/settings/api-keys
+- Grab your OpenAI API key from https://platform.openai.com/account/api-keys
+- Create a file named `.env` in this folder and include the following environment variables:
+
+```
+OPENAI_API_KEY=<your-api-key>
+AUTOBLOCKS_INGESTION_KEY=<your-ingestion-key>
+```
+
+<!-- getting started end -->
+
+## Create a prompt
+
+Go to https://app.autoblocks.ai/prompts and click on Create Prompt.
+
+![example-prompt](https://github.com/autoblocksai/autoblocks-examples/assets/7498009/acb7e17c-d09b-4ec0-9069-133bb50533e9)
+
+Create a prompt like this and deploy it.
+
+## Set your API key
+
+Then, get your API key from [here](https://app.autoblocks.ai/settings/api-keys) and set it as an environment variable:
+
+```bash
+export AUTOBLOCKS_API_KEY=...
+```
+
+## Create your `.autoblocks.yml` file
+
+This file instructs the CLI which prompts and which of their versions
+to autogenerate code for. One has already been created for you in [`./.autoblocks.yml`](./.autoblocks.yml).
+
+## Install dependencies
+
+```bash
+poetry install
+```
+
+## Autogenerate classes
+
+This CLI will autogenerate classes for you to use to interact with the prompts you've created in the UI.
+
+```
+poetry run autoblocks prompts generate
+```
+
+## Run the script
+
+```bash
+poetry run start
+```
---
+++ Python/openai-assistants-api/src/main-with-prompt-sdk.py
@@ -0,0 +1,106 @@
+import os
+import uuid
+
+import dotenv
+from autoblocks.tracer import AutoblocksTracer
+from openai import OpenAI
+from src.prompts import OpenaiAssistantsExamplePromptManager
+from src.prompts import OpenaiAssistantsExampleMinorVersion
+
+dotenv.load_dotenv(".env")
+
+client = OpenAI()
+
+tracer = AutoblocksTracer(
+    os.environ["AUTOBLOCKS_INGESTION_KEY"],
+    trace_id=str(uuid.uuid4()),
+    properties=dict(provider="openai"),
+)
+
+manager = OpenaiAssistantsExamplePromptManager(OpenaiAssistantsExampleMinorVersion.v0)
+
+
+def main_with_prompt_sdk():
+    print("Running example...")
+
+    with manager.exec() as prompt:
+        # Create the assistant
+        assistant_create_params = dict(
+            name="Math Tutor",
+            instructions=prompt.render.base_instructions(),
+            tools=[{"type": "code_interpreter"}],
+            model="gpt-4-turbo-preview",
+        )
+        tracer.send_event("ai.assistant.create", properties=assistant_create_params)
+        assistant = client.beta.assistants.create(**assistant_create_params)
+        # All future events will have the assistant_id and model properties
+        tracer.update_properties(dict(assistant_id=assistant.id, model=assistant.model))
+        tracer.send_event("ai.assistant.created")
+
+        # Create a new thread
+        tracer.send_event("ai.assistant.thread.create")
+        thread = client.beta.threads.create()
+        # All future events will have the thread_id property
+        tracer.update_properties(dict(thread_id=thread.id))
+        tracer.send_event("ai.assistant.thread.created")
+
+        # Create a user message
+        message_create_params = dict(
+            thread_id=thread.id,
+            role="user",
+            content=prompt.render.user(
+                question="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+            ),
+        )
+        tracer.send_event(
+            "ai.assistant.thread.message.create", properties=message_create_params
+        )
+        message = client.beta.threads.messages.create(**message_create_params)
+        tracer.send_event(
+            "ai.assistant.thread.message.created",
+            properties=dict(message_id=message.id),
+        )
+
+        # Run the assistant
+        run_params = dict(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+            instructions=prompt.render.response_instructions(
+                name="Jane Doe", account_level="premium"
+            ),
+        )
+        tracer.send_event("ai.assistant.thread.run.create", properties=run_params)
+        run = client.beta.threads.runs.create(**run_params)
+        # All future events will have the run_id property
+        tracer.update_properties(dict(run_id=run.id))
+        tracer.send_event("ai.assistant.thread.run.created")
+
+        run_completed = False
+
+        # Wait for run to complete
+        while not run_completed:
+            run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+            if run.status == "completed":
+                run_completed = True
+
+        # Log the steps of the run
+        run_steps = client.beta.threads.runs.steps.list(
+            thread_id=thread.id, run_id=run.id
+        )
+        for step in run_steps:
+            tracer.send_event(
+                f"ai.assistant.thread.run.step.{step.type}",
+                # Autoblocks uses run_ids and parent_run_ids to construct the trace tree view
+                properties=dict(run_id=step.id, parent_run_id=step.run_id),
+            )
+
+        # Log the output of the run
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        tracer.send_event(
+            "ai.assistant.thread.run.completed",
+            properties=dict(
+                response=messages.model_dump_json(), promptTracking=prompt.track()
+            ),
+        )
+
+    print(f"View your trace: https://app.autoblocks.ai/explore/trace/{tracer.trace_id}")
