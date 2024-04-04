deb7172bcbbbd864ff71f5b87428193128fb2514
Dan Morton <dan@autoblocks.ai>
2024-04-04 11:14:35 -0500

    JavaScript example

--- JavaScript/testing-sdk/src/run.ts
+++ JavaScript/testing-sdk/src/run.ts
@@ -2,5 +2,11 @@ import * as flashcardGenerator from './test-suites/flashcard-generator';
 import * as studyGuideOutline from './test-suites/study-guide-outline';
 
 (async () => {
-  await Promise.all([flashcardGenerator.run(), studyGuideOutline.run()]);
+  await Promise.all([
+    flashcardGenerator.run(),
+    studyGuideOutline.run(),
+    // Uncomment after setting up managed test cases
+    // for flashcard-generator
+    // flashcardGenerator.runWithManagedTestCases(),
+  ]);
 })();
--- JavaScript/testing-sdk/src/test-suites/flashcard-generator/index.ts
+++ JavaScript/testing-sdk/src/test-suites/flashcard-generator/index.ts
@@ -5,11 +5,34 @@ import {
   genFlashcardsFromNotes,
   type Flashcard,
 } from '../../tasks/flashcard-generator';
+import { AutoblocksAPIClient } from '@autoblocks/client';
 
 export async function run() {
   await runTestSuite<TestCase, Flashcard[]>({
     id: 'flashcard-generator',
     testCases: genTestCases(),
+    testCaseHash: ['notes'],
+    evaluators: [new IsProfessionalTone(), new IsSupportedByNotes()],
+    fn: (args: { testCase: TestCase }) =>
+      genFlashcardsFromNotes(args.testCase.notes),
+    maxTestCaseConcurrency: 5,
+  });
+}
+
+export async function runWithManagedTestCases() {
+  const client = new AutoblocksAPIClient();
+
+  const { testCases: managedTestCases } = await client.getTestCases<TestCase>({
+    testSuiteId: 'flashcard-generator',
+  });
+
+  const testCases: TestCase[] = managedTestCases.map(
+    (testCase) => testCase.body,
+  );
+
+  await runTestSuite<TestCase, Flashcard[]>({
+    id: 'flashcard-generator-managed',
+    testCases,
     testCaseHash: ['notes'],
     evaluators: [new IsProfessionalTone(), new IsSupportedByNotes()],
     fn: (args: { testCase: TestCase }) =>
