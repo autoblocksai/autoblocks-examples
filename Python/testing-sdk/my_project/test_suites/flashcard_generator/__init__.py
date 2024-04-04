953053325faae3b380c4b14248cac2c6b2e3cc34
Dan Morton <dan@autoblocks.ai>
2024-04-04 12:44:46 -0500

    cleanup

--- Python/testing-sdk/my_project/test_suites/flashcard_generator/__init__.py
+++ Python/testing-sdk/my_project/test_suites/flashcard_generator/__init__.py
@@ -26,28 +26,3 @@ def run():
         fn=test_fn,
         max_test_case_concurrency=5,
     )
-
-
-def run_with_managed_test_cases():
-    # Instantiate API client
-    client = AutoblocksAPIClient()
-
-    # Fetch test cases
-    test_cases_response = client.get_test_cases(test_suite_id="flashcard-generator")
-
-    # Build test cases array
-    test_cases = [
-        TestCase(**test_case.body) for test_case in test_cases_response.test_cases
-    ]
-
-    # Run test suite with managed test cases
-    run_test_suite(
-        id="flashcard-generator-managed",
-        test_cases=test_cases,
-        evaluators=[
-            IsSupportedByNotes(),
-            IsProfessionalTone(),
-        ],
-        fn=test_fn,
-        max_test_case_concurrency=5,
-    )
